6fa0ed2d2549a9ecf737b77c4d630badb1d8e88c
Duckie N <v.ducnv29@vinid.net>
2021-01-26 16:50:06 +0700

    Add sh gen project + internal libs deps

---
+++ Lib-Swift/Sources/StringProvider.swift
@@ -0,0 +1,12 @@
+
+public class StringProvider {
+    let baseString: String
+
+    public init(baseString: String) {
+        self.baseString = baseString
+    }
+
+    public func provideString() -> String {
+        return "\(baseString) was the base string"
+    }
+}
--- ios-app/AppDelegate.swift
+++ ios-app/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import AppLib
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
